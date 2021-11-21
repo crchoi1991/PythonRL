59afd25fa44447ada016bd32ebc6ba49ce90af56
lakshimi <maitte@naver.com>
2021-11-22 01:05:28 +0900

    Modify maze

--- maze.py
+++ maze.py
@@ -6,40 +6,37 @@ UP = 8
 RIGHT = 4
 DOWN = 2
 LEFT = 1
+MULTIPASS = 1
 
 # maze에 통로를 만드는 함수
-def mazesplit(maze, r, c, n, m):
+def mazesplit(maze, lt, rb):
     # 가로와 세로의 크기가 모두 1이면 종료
-    if n == 1 and m == 1: return
-    select = 0
-    if n == 1: select = 1
-    elif m == 1: select = 0
-    else: select = random.randrange(0, 2)
+    if lt == rb: return
     # 통로를 만들 선택이 세로인 경우
-    if select == 0:
-        u = random.randrange(0, n-1)
-        for _ in range(2):
-            v = random.randrange(0, m)
-            if (maze[u+r][v+c] & DOWN) == 0: continue
+    if lt[0] == rb[0] or (lt[1] != rb[1] and random.randrange(0, 2) == 0):
+        u = random.randrange(lt[1], rb[1])
+        for _ in range(MULTIPASS):
+            v = random.randrange(lt[0], rb[0]+1)
+            if (maze[u][v] & DOWN) == 0: continue
             # 위에 있는 칸에서는 아래쪽에 통로를 만들고
-            maze[u+r][v+c] -= DOWN
+            maze[u][v] -= DOWN
             # 아래에 있는 칸에서는 위쪽에 통로를 만듭니다.
-            maze[u+r+1][v+c] -= UP
+            maze[u+1][v] -= UP
         # 나누어진 칸에 대해서 mazesplit을 실행합니다.
-        mazesplit(maze, r, c, u+1, m)
-        mazesplit(maze, r+u+1, c, n-u-1, m)
+        mazesplit(maze, lt, (rb[0], u))
+        mazesplit(maze, (lt[0], u+1), rb)
     # 통로를 만들 선택이 가로인 경우
     else:
-        u = random.randrange(0, m-1)
-        for _ in range(2):
-            v = random.randrange(0, n)
-            if (maze[v+r][u+c] & RIGHT) == 0: continue
+        v = random.randrange(lt[0], rb[0])
+        for _ in range(MULTIPASS):
+            u = random.randrange(lt[1], rb[1]+1)
+            if (maze[u][v] & RIGHT) == 0: continue
             # 왼쪽에 있는 칸에서는 오른쪽에 통로를 만들고
-            maze[v+r][u+c] -= RIGHT
+            maze[u][v] -= RIGHT
             # 오른쪽에 있는 칸에서는 왼쪽에 통로를 만듭니다.
-            maze[v+r][u+c+1] -= LEFT
-        mazesplit(maze, r, c, n, u+1)
-        mazesplit(maze, r, c+u+1, n, m-u-1)
+            maze[u][v+1] -= LEFT
+        mazesplit(maze, lt, (v, rb[1]))
+        mazesplit(maze, (v+1, lt[1]), rb)
 
 def mazeprint(maze, n, m):
     for r in range(n):
@@ -62,7 +59,7 @@ print(n, m)
 maze = [ [15]*m for _ in range(n) ]
 
 # maze에 통로를 만듭니다.
-mazesplit(maze, 0, 0, n, m)
+mazesplit(maze, (0, 0), (m-1, n-1))
 
 # maze를 출력합니다.
 mazeprint(maze, n, m)

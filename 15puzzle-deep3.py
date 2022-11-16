d2fbd33058d4d99df59ec7871134fa64aba4d4e9
Aubrey Choi <lakshimi87@gmail.com>
2022-11-16 16:55:09 +0900

    update 15 puzzle.

--- 15puzzle-deep3.py
+++ 15puzzle-deep3.py
@@ -15,7 +15,7 @@ Cells = HCells*VCells
 CellSize = min(Width//HCells, Height//VCells)
 Black, White, Grey = (0, 0, 0), (250, 250, 250), (120, 120, 120)
 
-MaxShuffleCount = 50
+MaxShuffleCount = 60
 ShuffleCount = 10
 
 # puzzle class

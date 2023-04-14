e93351a5b6975729122aff138222f5fd61cf2f70
Aubrey Choi <lakshimi87@gmail.com>
2023-04-14 19:01:36 +0900

    update reversi shallow

--- reversi-shallow.py
+++ reversi-shallow.py
@@ -8,49 +8,22 @@ from reversiclient import ReversiClient
 
 cpPath = "reversi-shallow/cp_{0:06}.ckpt"
 
-def learn():
-	self.gameCount += 1
-	w, b = int(buf[:2]), int(buf[2:])
-	win = (w > b) - (w < b)
-	result = win+1 if self.turn == 1 else 1-win
-	winText = ("Lose", "Draw", "Win")
-	print(f"{winText[result]} W : {w}, B : {b}")
-	# 최종 보상을 선택 : 1 : 이겼다, 0 : 졌다, 0.5 : 비겼을 경우
-	reward = result-1
-	# 마지막 상태는 더 이상 값이 필요 없는 상태
-	self.episode[-1] = (self.episode[-1][0], reward)
-	# 학습을 위해서 데이터 처리
-	x, y = [], []
-	# 에피소드를 거꾸로 거슬러 올라가야한다.
-	for st, v in self.episode[::-1]:
-		rw = (1-self.alpha)*v + self.alpha*reward
-		x.append(st)
-		y.append(rw)
-		reward *= self.gamma
-	# 에피소드값을 이용하여 리플레이를 하도록 합니다.
-	self.replay(x, y)
-	return result
-
-def action(self, board):
+def getNextMove(model, board, turn):
 	# hints : 이번턴에서 놓을 수 있는 자리
-	hints = [i for i in range(64) if board[i] == "0"]
+	hints = ReversiClient.getHints(board)
 
 	# e-greedy에 의해 입실론값 확률로 랜덤하게 선택
-	if np.random.rand() <= self.epsilon:
-		r = random.choice(hints)
-		ret, nst = self.preRun(r)
-		if not ret: return None, -1, 0
-		v = self.model.predict(nst.reshape(1, 64), verbose=0)[0, 0]
-		return nst, r, v
+	if random.random() <= epsilon:
+		return random.choice(hints)
 
 	# 놓을 수 있는 자리 중 가장 높은 값을 주는 것을 선택
-	maxp, maxnst, maxv = -1, None, -100
+	maxp, maxv = -1, -100
 	for h in hints:
-		ret, nst = self.preRun(h)
-		if not ret: return None, -1, 0
-		v = self.model.predict(nst.reshape(1, 64), verbose=0)[0, 0]
-		if v > maxv: maxp, maxnst, maxv = h, nst, v
-	return nst, maxp, maxv
+		ReversiClient.prerun(board[:], h, turn)
+		state = getState(board, turn)
+		v = model.predict(state.reshape(1, 64), verbose=0)[0, 0]
+		if v > maxv: maxp, maxv = h, v
+	return maxp
 
 # 인공신경망 모델을 생성합니다.
 def buildModel():
@@ -66,7 +39,7 @@ def buildModel():
 					   optimizer=keras.optimizers.Adam())
 
 	# 학습한 데이터를 읽어서 모델에 적용합니다.
-	dir = os.path.dirname(Game.cpPath)
+	dir = os.path.dirname(cpPath)
 	latest = tf.train.latest_checkpoint(dir)
 	# 현재 학습한 것이 없는 경우는 무시토록 합니다.
 	if not latest: return model
@@ -81,13 +54,24 @@ def buildModel():
 	if epsilon < EpsilonMin: epsilon = EpsilonMin
 	return model
 
+def learn(model, result, episode):
+	# 최종 보상을 선택 : 1 : 이겼다, 0 : 졌다, 0.5 : 비겼을 경우
+	reward = result-1
+	# 학습을 위해서 데이터 처리
+	print(episode.shape)
+	y = model.predict(episode)
+	# 에피소드를 거꾸로 거슬러 올라가야한다.
+	for i in range(len(y)-1, -1, -1):
+		y[i] += Alpha*(reward-y[i])
+		reward *= -Gamma
+	# 에피소드값을 이용하여 리플레이를 하도록 합니다.
+	replay(model, episode, y)
+
 # 인공신경망으로 학습을 하기 위한 리플레이
 def replay(model, x, y):
-	xarray = np.array(x)
-	yarray = np.array(y)
-
+	global epsilon
 	# xarray 입력, yarray 출력을 이용하여 학습 진행
-	r = model.fit(xarray, yarray, epochs = Epochs, batch_size = BatchSize)
+	r = model.fit(x, y, epochs = Epochs, batch_size = BatchSize)
 
 	# e-greedy에서 입실론 값을 업데이트 합니다.
 	if epsilon >= EpsilonMin: epsilon *= EpsilonDecay
@@ -98,7 +82,10 @@ def replay(model, x, y):
 	print(f"Save weights {saveFile}")
 	model.save_weights(saveFile)
 		
-	
+def getState(board, turn):
+	ref = ( 0, 1 if turn==1 else -1, 1 if turn==2 else -1, 0 )
+	return np.array([ ref[x] for x in board ])
+
 # 머신러닝을 위한 파라미터들
 Epochs = 5
 BatchSize = 32
@@ -120,6 +107,7 @@ model = buildModel()
 win = [0]*3
 while True:
 	if not game.connect('localhost'): break
+	episode = np.empty((0, 64))
 	while True:
 		cmd, args = game.getEvent()
 		if cmd == None: continue
@@ -130,13 +118,21 @@ while True:
 			turn = args[1]
 			print(f'start a new game {gameCount+1} with color {Colors[turn]}')
 		elif cmd == 'ready':
-			place, _ = getNextMove(model, args[0], turn)
+			place = getNextMove(model, args[0], turn)
 			game.place(place)
+		elif cmd == 'place':
+			state = getState(args[0], args[2])
+			episode = np.append( episode, state.reshape(1, 64), axis=0 )
 		elif cmd == 'quit':
 			gameCount += 1
 			win[args[2]] += 1
-			print(f'turn : {Colors[turn]}, White : {args[0]}, Black : {args[1]}, {WinText[args[2]]}')
-			print(f'statistics : Lose : {win[0]}, Draw : {win[1]}, Win : {win[2]}, ratio : {win[2]*100/gameCount:.1f}%')
+			print(f'turn : {Colors[turn]},',
+				f'White : {args[0]}, Black : {args[1]},',
+				f'{WinText[args[2]]}')
+			print(f'statistics : Lose : {win[0]},',
+				f'Draw : {win[1]}, Win : {win[2]},',
+				f'ratio : {win[2]*100/gameCount:.1f}%')
+			learn(model, args[2], episode)
 			break
 		elif cmd == 'abort':
 			print(*args)
